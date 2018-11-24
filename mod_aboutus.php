46e10a46a258bef704aa967e1806b33132235ff8
shivendra kaushik <kaushik.shivendra@gmail.com>
2018-11-24 17:08:39 +0530

    Update mod_aboutus.php

--- mod_aboutus.php
+++ mod_aboutus.php
@@ -4,6 +4,7 @@
  * @copyright	@copyright	Copyright (c) 2018 3steechnosolutions. All rights reserved.
  * @license		http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
+ 
 
 // no direct access
 defined('_JEXEC') or die;
